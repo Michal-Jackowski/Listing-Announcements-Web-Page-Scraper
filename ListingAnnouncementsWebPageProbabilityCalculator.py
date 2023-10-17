949c856b16135498e01c76f35e8d84b1dfeeaae5
Michal-Jackowski <michaljacko@op.pl>
2023-10-18 01:56:27 +0200

    Functional Probability Calculator

--- ListingAnnouncementsWebPageProbabilityCalculator.py
+++ ListingAnnouncementsWebPageProbabilityCalculator.py
@@ -1,71 +1,75 @@
 import re
 import pandas as pd
 import path
-import datetime
+from datetime import datetime
+import calendar
 
-print("Probability Calculator data logic here.")
+def check_hour_probability(data):    
+    number_of_appearances_a_given_day = {"Monday": {"00:00": 0, "01:00": 0, "02:00": 0, "03:00": 0, "04:00": 0, "05:00": 0, "06:00": 0, "07:00": 0, "08:00": 0, "09:00": 0, "10:00": 0, "11:00": 0, "12:00": 0, "13:00": 0, "14:00": 0, "15:00": 0, "16:00": 0, "17:00": 0, "18:00": 0, "19:00": 0, "20:00": 0, "21:00": 0, "22:00": 0, "23:00": 0},
+                                         "Tuesday": {"00:00": 0, "01:00": 0, "02:00": 0, "03:00": 0, "04:00": 0, "05:00": 0, "06:00": 0, "07:00": 0, "08:00": 0, "09:00": 0, "10:00": 0, "11:00": 0, "12:00": 0, "13:00": 0, "14:00": 0, "15:00": 0, "16:00": 0, "17:00": 0, "18:00": 0, "19:00": 0, "20:00": 0, "21:00": 0, "22:00": 0, "23:00": 0},
+                                         "Wednesday": {"00:00": 0, "01:00": 0, "02:00": 0, "03:00": 0, "04:00": 0, "05:00": 0, "06:00": 0, "07:00": 0, "08:00": 0, "09:00": 0, "10:00": 0, "11:00": 0, "12:00": 0, "13:00": 0, "14:00": 0, "15:00": 0, "16:00": 0, "17:00": 0, "18:00": 0, "19:00": 0, "20:00": 0, "21:00": 0, "22:00": 0, "23:00": 0},
+                                         "Thursday": {"00:00": 0, "01:00": 0, "02:00": 0, "03:00": 0, "04:00": 0, "05:00": 0, "06:00": 0, "07:00": 0, "08:00": 0, "09:00": 0, "10:00": 0, "11:00": 0, "12:00": 0, "13:00": 0, "14:00": 0, "15:00": 0, "16:00": 0, "17:00": 0, "18:00": 0, "19:00": 0, "20:00": 0, "21:00": 0, "22:00": 0, "23:00": 0},
+                                         "Friday": {"00:00": 0, "01:00": 0, "02:00": 0, "03:00": 0, "04:00": 0, "05:00": 0, "06:00": 0, "07:00": 0, "08:00": 0, "09:00": 0, "10:00": 0, "11:00": 0, "12:00": 0, "13:00": 0, "14:00": 0, "15:00": 0, "16:00": 0, "17:00": 0, "18:00": 0, "19:00": 0, "20:00": 0, "21:00": 0, "22:00": 0, "23:00": 0},
+                                         "Saturday": {"00:00": 0, "01:00": 0, "02:00": 0, "03:00": 0, "04:00": 0, "05:00": 0, "06:00": 0, "07:00": 0, "08:00": 0, "09:00": 0, "10:00": 0, "11:00": 0, "12:00": 0, "13:00": 0, "14:00": 0, "15:00": 0, "16:00": 0, "17:00": 0, "18:00": 0, "19:00": 0, "20:00": 0, "21:00": 0, "22:00": 0, "23:00": 0},
+                                         "Sunday": {"00:00": 0, "01:00": 0, "02:00": 0, "03:00": 0, "04:00": 0, "05:00": 0, "06:00": 0, "07:00": 0, "08:00": 0, "09:00": 0, "10:00": 0, "11:00": 0, "12:00": 0, "13:00": 0, "14:00": 0, "15:00": 0, "16:00": 0, "17:00": 0, "18:00": 0, "19:00": 0, "20:00": 0, "21:00": 0, "22:00": 0, "23:00": 0}}
 
-# DATE AGGREGATION 
-# EXAMPLE 2023-07-24 09:02
+    for x in range(len(data)):
+        date = data["Date"][x]
+        datetime_object = datetime.strptime(date, '%Y-%m-%d %H:%M')
+        day_of_week = calendar.day_name[datetime_object.weekday()]
+        if re.search("00:", date, re.IGNORECASE):                
+            number_of_appearances_a_given_day[day_of_week]["00:00"] += 1
+        elif re.search("01:", date, re.IGNORECASE):
+            number_of_appearances_a_given_day[day_of_week]["01:00"] += 1
+        elif re.search("02:", date, re.IGNORECASE):
+            number_of_appearances_a_given_day[day_of_week]["02:00"] += 1
+        elif re.search("03:", date, re.IGNORECASE):
+            number_of_appearances_a_given_day[day_of_week]["03:00"] += 1
+        elif re.search("04:", date, re.IGNORECASE):
+            number_of_appearances_a_given_day[day_of_week]["04:00"] += 1
+        elif re.search("05:", date, re.IGNORECASE):
+            number_of_appearances_a_given_day[day_of_week]["05:00"] += 1
+        elif re.search("06:", date, re.IGNORECASE):
+            number_of_appearances_a_given_day[day_of_week]["06:00"] += 1
+        elif re.search("07:", date, re.IGNORECASE):
+            number_of_appearances_a_given_day[day_of_week]["07:00"] += 1
+        elif re.search("08:", date, re.IGNORECASE):
+            number_of_appearances_a_given_day[day_of_week]["08:00"] += 1
+        elif re.search("09:", date, re.IGNORECASE):
+            number_of_appearances_a_given_day[day_of_week]["09:00"] += 1
+        elif re.search("10:", date, re.IGNORECASE):
+            number_of_appearances_a_given_day[day_of_week]["10:00"] += 1
+        elif re.search("11:", date, re.IGNORECASE):
+            number_of_appearances_a_given_day[day_of_week]["11:00"] += 1
+        elif re.search("12:", date, re.IGNORECASE):
+            number_of_appearances_a_given_day[day_of_week]["12:00"] += 1
+        elif re.search("13:", date, re.IGNORECASE):
+            number_of_appearances_a_given_day[day_of_week]["13:00"] += 1
+        elif re.search("14:", date, re.IGNORECASE):
+            number_of_appearances_a_given_day[day_of_week]["14:00"] += 1
+        elif re.search("15:", date, re.IGNORECASE):
+            number_of_appearances_a_given_day[day_of_week]["15:00"] += 1
+        elif re.search("16:", date, re.IGNORECASE):
+            number_of_appearances_a_given_day[day_of_week]["16:00"] += 1
+        elif re.search("17:", date, re.IGNORECASE):
+            number_of_appearances_a_given_day[day_of_week]["17:00"] += 1
+        elif re.search("18:", date, re.IGNORECASE):
+            number_of_appearances_a_given_day[day_of_week]["18:00"] += 1
+        elif re.search("19:", date, re.IGNORECASE):
+            number_of_appearances_a_given_day[day_of_week]["19:00"] += 1
+        elif re.search("20:", date, re.IGNORECASE):
+            number_of_appearances_a_given_day[day_of_week]["20:00"] += 1
+        elif re.search("21:", date, re.IGNORECASE):
+            number_of_appearances_a_given_day[day_of_week]["21:00"] += 1
+        elif re.search("22:", date, re.IGNORECASE):
+            number_of_appearances_a_given_day[day_of_week]["22:00"] += 1
+        elif re.search("23:", date, re.IGNORECASE):
+            number_of_appearances_a_given_day[day_of_week]["23:00"] += 1
+    return number_of_appearances_a_given_day
+
 df_spot = pd.read_excel(path.filtered_data_from_webpage_spot_excel)
-
-number_of_00_hour, number_of_01_hour, number_of_02_hour, number_of_03_hour, number_of_04_hour, number_of_05_hour, number_of_06_hour, number_of_07_hour = 0, 0, 0, 0, 0, 0, 0, 0
-number_of_08_hour, number_of_09_hour, number_of_10_hour, number_of_11_hour, number_of_12_hour, number_of_13_hour, number_of_14_hour, number_of_15_hour = 0, 0, 0, 0, 0, 0, 0, 0
-number_of_16_hour, number_of_17_hour, number_of_18_hour, number_of_19_hour, number_of_20_hour, number_of_21_hour, number_of_22_hour, number_of_23_hour = 0, 0, 0, 0, 0, 0, 0, 0
-
-for x in range(len(df_spot)):
-    date = df_spot["Date"][x]
-    if re.search("00:", date, re.IGNORECASE):
-        number_of_00_hour += 1
-    elif re.search("01:", date, re.IGNORECASE):
-        number_of_01_hour += 1
-    elif re.search("02:", date, re.IGNORECASE):
-        number_of_02_hour += 1
-    elif re.search("03:", date, re.IGNORECASE):
-        number_of_03_hour += 1
-    elif re.search("04:", date, re.IGNORECASE):
-        number_of_04_hour += 1
-    elif re.search("05:", date, re.IGNORECASE):
-        number_of_05_hour += 1
-    elif re.search("06:", date, re.IGNORECASE):
-        number_of_06_hour += 1
-    elif re.search("07:", date, re.IGNORECASE):
-        number_of_07_hour += 1
-    elif re.search("08:", date, re.IGNORECASE):
-        number_of_08_hour += 1
-    elif re.search("09:", date, re.IGNORECASE):
-        number_of_09_hour += 1
-    elif re.search("10:", date, re.IGNORECASE):
-        number_of_10_hour += 1
-    elif re.search("11:", date, re.IGNORECASE):
-        number_of_11_hour += 1
-    elif re.search("12:", date, re.IGNORECASE):
-        number_of_12_hour += 1
-    elif re.search("13:", date, re.IGNORECASE):
-        number_of_13_hour += 1
-    elif re.search("14:", date, re.IGNORECASE):
-        number_of_14_hour += 1
-    elif re.search("15:", date, re.IGNORECASE):
-        number_of_15_hour += 1
-    elif re.search("16:", date, re.IGNORECASE):
-        number_of_16_hour += 1
-    elif re.search("17:", date, re.IGNORECASE):
-        number_of_17_hour += 1
-    elif re.search("18:", date, re.IGNORECASE):
-        number_of_18_hour += 1
-    elif re.search("19:", date, re.IGNORECASE):
-        number_of_19_hour += 1
-    elif re.search("20:", date, re.IGNORECASE):
-        number_of_20_hour += 1
-    elif re.search("21:", date, re.IGNORECASE):
-        number_of_21_hour += 1
-    elif re.search("22:", date, re.IGNORECASE):
-        number_of_22_hour += 1
-    elif re.search("23:", date, re.IGNORECASE):
-        number_of_23_hour += 1
-
-hours_list = {"00:00": number_of_00_hour, "01:00": number_of_01_hour, "02:00": number_of_02_hour, "03:00": number_of_03_hour, "04:00": number_of_04_hour, "05:00": number_of_05_hour, "06:00": number_of_06_hour, "07:00": number_of_07_hour,
-              "08:00": number_of_08_hour, "09:00": number_of_09_hour, "10:00": number_of_10_hour, "11:00": number_of_11_hour, "12:00": number_of_12_hour, "13:00": number_of_13_hour, "14:00": number_of_14_hour, "15:00": number_of_15_hour,
-              "16:00": number_of_16_hour, "17:00": number_of_17_hour, "18:00": number_of_18_hour, "19:00": number_of_19_hour, "20:00": number_of_20_hour, "21:00": number_of_21_hour, "22:00": number_of_22_hour, "23:00": number_of_23_hour}
-
-print("All hours = " + str(hours_list))
+df_futures = pd.read_excel(path.filtered_data_from_webpage_futures_excel)
+df_others = pd.read_excel(path.filtered_data_from_webpage_others_excel)
+print("df_spot = " + str(check_hour_probability(df_spot)))
+print("df_futures = " + str(check_hour_probability(df_futures)))
+print("df_others = " + str(check_hour_probability(df_others)))
---
+++ fix_asap.py
@@ -0,0 +1,3 @@
+# generate two excel additional tables
+# input data from generator to excel tables
+# change int values to probability written as e.g 0.51% in range 0 - 100%
